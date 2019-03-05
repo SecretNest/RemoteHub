d0c0211ceab2d45ec68db2b2ca37e5648e4180f1
Allen Cui <allen_st_clair@msn.com>
2019-03-05 12:55:35 +0800

    Add switch

---
+++ Source/Code/Code.RemoteHub.Switch/AdatperPackage.cs
@@ -0,0 +1,19 @@
+﻿using SecretNest.RemoteHub;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code.RemoteHub.Switch
+{
+    class AdatperPackage
+    {
+        IRemoteHubAdapter remoteHubAdapter;
+        public IRemoteHubAdapter RemoteHubAdapter => remoteHubAdapter;
+
+        public AdatperPackage(IRemoteHubAdapter remoteHubAdapter)
+        {
+            this.remoteHubAdapter = remoteHubAdapter;
+
+        }
+    }
+}
---
+++ Source/Code/Code.RemoteHub.Switch/RemoteHubSwitch.cs
@@ -0,0 +1,57 @@
+﻿using SecretNest.RemoteHub;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code.RemoteHub.Switch
+{
+    public class RemoteHubSwitch
+    {
+        Dictionary<IRemoteHubAdapter, AdatperPackage> adapterPackages = new Dictionary<IRemoteHubAdapter, AdatperPackage>();
+        Dictionary<Guid, List<AdatperPackage>> adapterOfClients = new Dictionary<Guid, List<AdatperPackage>>();
+
+        public IEnumerable<Guid> GetAllClients()
+        {
+            throw new Exception();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void AddAdapter(IRemoteHubAdapter adapter)
+        {
+
+        }
+
+        public void AddAdapters(params IRemoteHubAdapter[] adapters)
+        {
+            if (adapters != null && adapters.Length > 0)
+                foreach (var adapter in adapters)
+                    AddAdapter(adapter);
+        }
+
+        public void RemoveAdapter(IRemoteHubAdapter adapter)
+        {
+
+        }
+
+        public void RemoveAdapters(params IRemoteHubAdapter[] adapters)
+        {
+            if (adapters != null && adapters.Length > 0)
+                foreach (var adapter in adapters)
+                    RemoveAdapter(adapter);
+        }
+
+        public void RemoveAllAdapters()
+        {
+
+        }
+    }
+}
